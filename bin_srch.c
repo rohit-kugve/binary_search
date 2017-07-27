3056bb7494e66e9ff43d931d1bda7428bf8011ab
rohit kugve <rkugver1@binghamton.edu>
2017-07-27 12:48:25 -0400

    replced tab with spaces. works in gedit

--- bin_srch.c
+++ bin_srch.c
@@ -42,31 +42,31 @@ int compare_float(void *a, void *b)
     float y = *(float*)b;
 
     if (fabs(x - y) <= FLT_EPSILON){
-        return 0;						//diff is smallar than epsilon so consider them equal
+        return 0;                        //diff is smallar than epsilon so consider them equal
     }
     if (x > y){
         return 1;
     }
-    return -1;			//x < y
+    return -1;            //x < y
 }
 
 
 /* bin_srch - does binary search, not to be called directly, usr wraper function binary_search()
- * parameters and return same as binary_search()	 
+ * parameters and return same as binary_search()     
  */ 
 static int bin_srch(void *arr, int d_size, int s, int e, void *key, int (*compare_fn)(void*, void*))
 {
     int mid = 0, cmp = 0;
     if (s > e){
-        return -1;									//key not found
+        return -1;                                    //key not found
     }
-    mid = (s + e) / 2;								//get mid point of array
-    cmp = compare_fn(arr + (mid * d_size), key);	//do not use == to compare since it might not work for some datatypes like float
+    mid = (s + e) / 2;                                //get mid point of array
+    cmp = compare_fn(arr + (mid * d_size), key);    //do not use == to compare since it might not work for some datatypes like float
     if (cmp == 0)      
-        return mid;									//key found in arr[mid]
-    if (cmp > 0)									
-        return bin_srch(arr, d_size, s, mid - 1, key, compare_fn);	//arr[mid] > key, key is in left half
-    return bin_srch(arr, d_size, mid + 1, e, key, compare_fn);		//arr[mid] < key, key is in right half
+        return mid;                                    //key found in arr[mid]
+    if (cmp > 0)                                    
+        return bin_srch(arr, d_size, s, mid - 1, key, compare_fn);    //arr[mid] > key, key is in left half
+    return bin_srch(arr, d_size, mid + 1, e, key, compare_fn);        //arr[mid] < key, key is in right half
 }
 
 /* binary_search - search for key in an array  of any data type using binary search logic. 
@@ -83,9 +83,9 @@ static int bin_srch(void *arr, int d_size, int s, int e, void *key, int (*compar
  */
 int binary_search(void *array, int arr_size, int data_size, void *key, int (*compare_fn)(void*, void*))
 {   
-    int n = 0;	// no. of elements in array
+    int n = 0;    // no. of elements in array
     if (array == NULL || arr_size <= 0 || data_size <=0 || key == NULL || compare_fn == NULL){
-        return -2;							//some sanity checking
+        return -2;                            //some sanity checking
     }
     n = (arr_size / data_size); 
     return bin_srch(array, data_size, 0, n - 1 , key, compare_fn); //call helper function
@@ -94,42 +94,42 @@ int binary_search(void *array, int arr_size, int data_size, void *key, int (*com
 /* test cases and main function */
 int test_int(void)
 {
-	int i_a[] = {0, 4, 8, 9, 22};
+    int i_a[] = {0, 4, 8, 9, 22};
     int i_key = 22;
-	printf("expected = 4, result = %d\n", binary_search(i_a, sizeof(i_a), sizeof(int), &i_key, &compare_int));
+    printf("expected = 4, result = %d\n", binary_search(i_a, sizeof(i_a), sizeof(int), &i_key, &compare_int));
 }
 
 int test_float(void)
 {
-	//test odd elements
+    //test odd elements
     float f1[] = {0.2f, 0.4f, 0.8f, 1.9f, 2.22f};
     float key1 = 1.9f;
-	printf("expected = 3, result = %d\n", binary_search(f1, sizeof(f1), sizeof(float), &key1, &compare_float));	
-	//test even elements
-	float f2[] = {0.2f, 0.4f, 0.8f, 1.9f, 2.22, 2.40};
+    printf("expected = 3, result = %d\n", binary_search(f1, sizeof(f1), sizeof(float), &key1, &compare_float));    
+    //test even elements
+    float f2[] = {0.2f, 0.4f, 0.8f, 1.9f, 2.22, 2.40};
     float key2 = 0.2f;
-	printf("expected = 0, result = %d\n", binary_search(f2, sizeof(f2), sizeof(float), &key2, &compare_float));	
-	//test 0 elements
-	float f3[] = {};
+    printf("expected = 0, result = %d\n", binary_search(f2, sizeof(f2), sizeof(float), &key2, &compare_float));    
+    //test 0 elements
+    float f3[] = {};
     float key3 = 0.2f;
-	printf("expected = -2, result = %d\n", binary_search(f3, sizeof(f3), sizeof(float), &key3, &compare_float));
-	//test key not present
-	float f4[] = {0.2f, 0.4f, 0.8f, 1.9f, 2.22, 2.40};
+    printf("expected = -2, result = %d\n", binary_search(f3, sizeof(f3), sizeof(float), &key3, &compare_float));
+    //test key not present
+    float f4[] = {0.2f, 0.4f, 0.8f, 1.9f, 2.22, 2.40};
     float key4 = 0.9f;
-	printf("expected = -1, result = %d\n", binary_search(f4, sizeof(f4), sizeof(float), &key4, &compare_float));
-	//test small diff numbers
-	//even though the key is not in array, due to epsilon comparision, it picks index 2 as key.
-	float f5[] = {0.20000001f, 0.20000004f, 0.20000006f, 0.20000008f, 2.22, 2.40};
+    printf("expected = -1, result = %d\n", binary_search(f4, sizeof(f4), sizeof(float), &key4, &compare_float));
+    //test small diff numbers
+    //even though the key is not in array, due to epsilon comparision, it picks index 2 as key.
+    float f5[] = {0.20000001f, 0.20000004f, 0.20000006f, 0.20000008f, 2.22, 2.40};
     float key5 = 0.20000005f;
-	printf("expected = -1, result = %d\n", binary_search(f5, sizeof(f5), sizeof(float), &key5, &compare_float));
-	
-	return 0;
+    printf("expected = -1, result = %d\n", binary_search(f5, sizeof(f5), sizeof(float), &key5, &compare_float));
+    
+    return 0;
 }
 
 int main(void)
 {
-	test_int();
-	test_float();
+    test_int();
+    test_float();
     return 0;
 }
 
